74048608f6e713229c8ced64bdeac7e587b8ba76
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-06 22:08:12 +0900

    feat: add navigation component

--- app/src/main/java/jp/kaleidot725/orbit/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/orbit/MainActivity.kt
@@ -3,6 +3,9 @@ package jp.kaleidot725.orbit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import jp.kaleidot725.orbit.ui.page.library.PokemonLibraryPage
 import jp.kaleidot725.orbit.ui.theme.OrbitTheme
 import org.koin.androidx.viewmodel.ext.android.getViewModel
@@ -12,7 +15,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OrbitTheme {
-                PokemonLibraryPage(viewModel = getViewModel())
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "library") {
+                    composable("library") {
+                        PokemonLibraryPage(viewModel = getViewModel())
+                    }
+                }
             }
         }
     }
