d76a11b93b59e892f7f38796164039b5cf0815dd
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-10 10:39:24 +0900

    feat: update PokemonList

--- app/src/main/java/jp/kaleidot725/orbit/ui/organisms/PokemonList.kt
+++ app/src/main/java/jp/kaleidot725/orbit/ui/organisms/PokemonList.kt
@@ -1,8 +1,10 @@
 package jp.kaleidot725.orbit.ui.organisms
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,9 +13,13 @@ import jp.kaleidot725.orbit.data.entity.PokemonDetails
 import jp.kaleidot725.orbit.ui.SAMPLE_POKEMON_DETAILS_LIST
 import jp.kaleidot725.orbit.ui.atoms.PokemonCard
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PokemonList(details: List<PokemonDetails>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(minSize = 128.dp),
+        modifier = modifier
+    ) {
         details.forEach { details ->
             item {
                 PokemonCard(
