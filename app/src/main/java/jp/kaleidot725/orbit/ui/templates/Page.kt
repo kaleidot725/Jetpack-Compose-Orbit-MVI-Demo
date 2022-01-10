5b5b94e85a8c2f21e84fa980ecbddabb71cf1d0e
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-10 10:52:21 +0900

    feat: create Page template

---
+++ app/src/main/java/jp/kaleidot725/orbit/ui/templates/Page.kt
@@ -0,0 +1,48 @@
+package jp.kaleidot725.orbit.ui.templates
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Page(
+    header: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Column {
+            header()
+            content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Page_Preview() {
+    Page(
+        header = {
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Red)
+                    .height(75.dp)
+                    .fillMaxWidth()
+            )
+        },
+        content = {
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Blue)
+                    .fillMaxSize()
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(800.dp)
+    )
+}
