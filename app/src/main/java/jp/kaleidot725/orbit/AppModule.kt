6773a4292bca8154ebdc7530575a4f6be692d20c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-22 00:16:46 +0900

    feat: add koin extention

--- app/src/main/java/jp/kaleidot725/orbit/AppModule.kt
+++ app/src/main/java/jp/kaleidot725/orbit/AppModule.kt
@@ -1,9 +1,36 @@
 package jp.kaleidot725.orbit
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import jp.kaleidot725.orbit.ui.pages.details.DetailsViewModel
 import jp.kaleidot725.orbit.ui.pages.library.LibraryViewModel
+import org.koin.androidx.viewmodel.ViewModelOwner
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.koin.getViewModel
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
 import org.koin.dsl.module
+import org.koin.java.KoinJavaComponent
+
+@Composable
+fun getComposeViewModelOwner(): ViewModelOwner {
+    return ViewModelOwner.from(
+        LocalViewModelStoreOwner.current!!,
+        LocalSavedStateRegistryOwner.current
+    )
+}
+
+@Composable
+inline fun <reified T : ViewModel> getComposeViewModel(
+    qualifier: Qualifier? = null,
+    noinline parameters: ParametersDefinition? = null,
+): T {
+    val viewModelOwner = getComposeViewModelOwner()
+    return KoinJavaComponent.getKoin().getViewModel(qualifier, { viewModelOwner }, parameters)
+}
+
 
 val appModule = module {
     viewModel {
