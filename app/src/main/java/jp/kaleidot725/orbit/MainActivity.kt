498dfa46b7fed3ef681aa3965adcbe5a4d7b8884
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-09-29 22:32:57 +0900

    Modify orbit mvi code

--- app/src/main/java/jp/kaleidot725/orbit/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/orbit/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.primarySurface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
@@ -29,6 +27,8 @@ import jp.kaleidot725.orbit.ui.components.pages.library.LibraryViewModel
 import jp.kaleidot725.orbit.ui.screen.Screen
 import jp.kaleidot725.orbit.ui.theme.OrbitTheme
 import org.koin.core.parameter.parametersOf
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,18 +52,14 @@ class MainActivity : ComponentActivity() {
 private fun NavGraphBuilder.addInit(navController: NavController) {
     composable(route = Screen.Init.route) {
         val viewModel = getComposeViewModel<InitViewModel>()
-        val state by viewModel.container.stateFlow.collectAsState()
-
-        LaunchedEffect(viewModel) {
-            viewModel.container.sideEffectFlow.collect {
-                when (it) {
-                    is InitSideEffect.Completed -> {
-                        navController.navigate(route = Screen.Library.route)
-                    }
+        val state by viewModel.collectAsState()
+        viewModel.collectSideEffect {
+            when (it) {
+                is InitSideEffect.Completed -> {
+                    navController.navigate(route = Screen.Library.route)
                 }
             }
         }
-
         InitPage(
             state = state,
             onRetry = { viewModel.retry() }
@@ -74,18 +70,14 @@ private fun NavGraphBuilder.addInit(navController: NavController) {
 private fun NavGraphBuilder.addLibrary(navController: NavController) {
     composable(route = Screen.Library.route) {
         val viewModel = getComposeViewModel<LibraryViewModel>()
-        val state by viewModel.container.stateFlow.collectAsState()
-
-        LaunchedEffect(viewModel) {
-            viewModel.container.sideEffectFlow.collect { effect ->
-                when (effect) {
-                    is LibrarySideEffect.ShowDetails -> {
-                        navController.navigate(route = Screen.Details.createRoute(effect.id))
-                    }
+        val state by viewModel.collectAsState()
+        viewModel.collectSideEffect {
+            when (it) {
+                is LibrarySideEffect.ShowDetails -> {
+                    navController.navigate(route = Screen.Details.createRoute(it.id))
                 }
             }
         }
-
         LibraryPage(
             state,
             onShowDetail = { id -> viewModel.showDetails(id) },
@@ -96,17 +88,12 @@ private fun NavGraphBuilder.addLibrary(navController: NavController) {
 
 private fun NavGraphBuilder.addDetails(navController: NavController) {
     composable(route = Screen.Details.route) {
-        val viewModel = getComposeViewModel<DetailsViewModel>(
-            parameters = { parametersOf(Screen.Details.getArgumentId(it)) }
-        )
-        val state by viewModel.container.stateFlow.collectAsState()
-
-        LaunchedEffect(viewModel) {
-            viewModel.container.sideEffectFlow.collect {
-                when (it) {
-                    is DetailsSideEffect.Back -> {
-                        navController.popBackStack()
-                    }
+        val viewModel = getComposeViewModel<DetailsViewModel>(parameters = { parametersOf(Screen.Details.getArgumentId(it)) })
+        val state by viewModel.collectAsState()
+        viewModel.collectSideEffect {
+            when (it) {
+                is DetailsSideEffect.Back -> {
+                    navController.popBackStack()
                 }
             }
         }
